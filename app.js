4a6a8c6f1d2b5dc5eff427be1c3d2cf736022160
Daniel <danielsaraldi@gmail.com>
2021-04-17 13:56:33 -0300

    refactor: move alert to helper method and concerts bug of conversion undefined to array

--- app.js
+++ app.js
@@ -28,6 +28,31 @@ const getErrorMessage = (errorType) =>
       "Nível do plano não compatível com esse tipo de solicitação!",
   }[errorType] || "Não foi possível obter as informações!");
 
+const showAlert = (err) => {
+  const div = document.createElement("div");
+  const button = document.createElement("button");
+
+  div.textContent = err.message;
+  div.classList.add(
+    "alert",
+    "alert-warning",
+    "alert-dismissible",
+    "fade",
+    "show"
+  );
+  div.setAttribute("role", "alert");
+  button.classList.add("btn-close");
+  button.setAttribute("type", "button");
+  button.setAttribute("aria-label", "Close");
+  button.setAttribute("data-bs-dismiss", "alert");
+
+  button.addEventListener("click", () => div.remove());
+
+  div.appendChild(button);
+  // Add alert after the currencies
+  currenciesEl.insertAdjacentElement("afterend", div);
+};
+
 const fetchExchangeRate = async (url) => {
   try {
     const response = await fetch(url);
@@ -44,52 +69,33 @@ const fetchExchangeRate = async (url) => {
 
     return exchangeRateData;
   } catch (err) {
-    const div = document.createElement("div");
-    const button = document.createElement("button");
-
-    div.textContent = err.message;
-    div.classList.add(
-      "alert",
-      "alert-warning",
-      "alert-dismissible",
-      "fade",
-      "show"
-    );
-    div.setAttribute("role", "alert");
-    button.classList.add("btn-close");
-    button.setAttribute("type", "button");
-    button.setAttribute("aria-label", "Close");
-    button.setAttribute("data-bs-dismiss", "alert");
-
-    button.addEventListener("click", () => div.remove());
-
-    div.appendChild(button);
-    // Add alert after the currencies
-    currenciesEl.insertAdjacentElement("afterend", div);
+    showAlert(err);
   }
 };
 
 const init = async () => {
   internalExchangeRate = { ...(await fetchExchangeRate(getUrl("USD"))) };
 
-  // Return name property of the object
-  const getOptions = (selectedCurrency) =>
-    Object.keys(internalExchangeRate.conversion_rates)
-      .map(
-        (currency) =>
-          `<option ${
-            currency === selectedCurrency ? "selected" : ""
-          }>${currency}</option>`
-      )
-      .join("");
+  if (internalExchangeRate.conversion_rates) {
+    // Return name property of the object
+    const getOptions = (selectedCurrency) =>
+      Object.keys(internalExchangeRate.conversion_rates)
+        .map(
+          (currency) =>
+            `<option ${
+              currency === selectedCurrency ? "selected" : ""
+            }>${currency}</option>`
+        )
+        .join("");
 
-  currencyOneEl.innerHTML = getOptions("USD");
-  currencyTwoEl.innerHTML = getOptions("BRL");
+    currencyOneEl.innerHTML = getOptions("USD");
+    currencyTwoEl.innerHTML = getOptions("BRL");
 
-  convertedValueEl.textContent = internalExchangeRate.conversion_rates.BRL.toFixed(
-    2
-  );
-  valuePrecisionEl.textContent = `1 USD = ${internalExchangeRate.conversion_rates.BRL} BRL`;
+    convertedValueEl.textContent = internalExchangeRate.conversion_rates.BRL.toFixed(
+      2
+    );
+    valuePrecisionEl.textContent = `1 USD = ${internalExchangeRate.conversion_rates.BRL} BRL`;
+  }
 };
 
 timesCurrencyOneEl.addEventListener("input", (e) => {
